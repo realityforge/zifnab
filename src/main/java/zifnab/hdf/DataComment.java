255f191593ab171f074aaeaa64a744ed02482688
Peter Donald <peter@realityforge.org>
2019-08-05 16:12:41 +1000

    Add support for comments

---
+++ src/main/java/zifnab/hdf/DataComment.java
@@ -0,0 +1,50 @@
+package zifnab.hdf;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Objects;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+public final class DataComment
+  extends DataNode
+{
+  @Nullable
+  private final DataElement _parent;
+  @Nonnull
+  private final String _comment;
+
+  public DataComment( @Nullable final DataElement parent, @Nonnull final String comment )
+  {
+    assert !comment.isEmpty();
+    assert !comment.contains( "\n" );
+    _parent = parent;
+    _comment = Objects.requireNonNull( comment );
+    if ( null != _parent )
+    {
+      _parent.append( this );
+    }
+  }
+
+  @Nullable
+  public DataElement getParent()
+  {
+    return _parent;
+  }
+
+  @Nonnull
+  public String getComment()
+  {
+    return _comment;
+  }
+
+  @Override
+  void write( @Nonnull final Writer writer, final int depth )
+    throws IOException
+  {
+    writeIndent( writer, depth );
+    writer.write( "# " );
+    writer.write( _comment );
+    writer.write( '\n' );
+  }
+}
---
+++ src/test/java/zifnab/hdf/test/DataCommentTest.java
@@ -0,0 +1,96 @@
+package zifnab.hdf.test;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import javax.annotation.Nonnull;
+import org.realityforge.guiceyloops.shared.ValueUtil;
+import org.testng.annotations.Test;
+import zifnab.AbstractTest;
+import zifnab.hdf.DataComment;
+import zifnab.hdf.DataDocument;
+import zifnab.hdf.DataElement;
+import zifnab.hdf.DataFile;
+import zifnab.hdf.DataNode;
+import static org.testng.Assert.*;
+
+public class DataCommentTest
+  extends AbstractTest
+{
+  @Test
+  public void constructTopLevel()
+  {
+    final String comment = ValueUtil.randomString();
+    final DataComment element = new DataComment( null, comment );
+
+    assertNull( element.getParent() );
+    assertEquals( element.getComment(), comment );
+  }
+
+  @Test
+  public void constructNested()
+  {
+    final DataElement parent = new DataElement( null, "planet", "AK5" );
+
+    final String comment = ValueUtil.randomString();
+    final DataComment child = new DataComment( parent, comment );
+
+    assertEquals( child.getParent(), parent );
+    assertEquals( child.getComment(), comment );
+  }
+
+  @Test
+  public void writeAtTopLevel()
+    throws Exception
+  {
+    final String comment = "Hello darkness, my old friend!";
+    final DataComment node = new DataComment( null, comment );
+
+    final String output = writeElement( node );
+    assertEquals( output, "# Hello darkness, my old friend!\n" );
+  }
+
+  @Test
+  public void writeWithMultipleLayers()
+    throws Exception
+  {
+    final DataComment comment1 = new DataComment( null, "The humanitarian mission!" );
+    final DataElement element1 = new DataElement( null, "mission", "Drought Relief" );
+    new DataComment( element1, "The name of the mission as presented to user" );
+    new DataElement( element1, "name", "Drought relief to <planet>" );
+    final DataElement offer = new DataElement( element1, "to", "offer" );
+    new DataComment( offer, "This happens 1 in 10 times?" );
+    new DataElement( offer, "random", "<", "10" );
+
+    final DataComment comment2 = new DataComment( null, "The safe planet" );
+    final DataComment comment3 = new DataComment( null, "... or so I hear" );
+    final DataElement element2 = new DataElement( null, "planet", "Dune" );
+
+    final String output = writeElement( comment1, element1, comment2, comment3, element2 );
+    assertEquals( output,
+                  "# The humanitarian mission!\n" +
+                  "mission \"Drought Relief\"\n" +
+                  "\t# The name of the mission as presented to user\n" +
+                  "\tname \"Drought relief to <planet>\"\n" +
+                  "\tto offer\n" +
+                  "\t\t# This happens 1 in 10 times?\n" +
+                  "\t\trandom < 10\n" +
+                  "\n" +
+                  "# The safe planet\n" +
+                  "# ... or so I hear\n" +
+                  "planet Dune\n" );
+  }
+
+  @Nonnull
+  private String writeElement( @Nonnull final DataNode... nodes )
+    throws IOException
+  {
+    final Path file = createTempDataFile();
+    final DataDocument document = new DataDocument();
+    for ( final DataNode node : nodes )
+    {
+      document.append( node );
+    }
+    new DataFile( file, document ).write();
+    return readContent( file );
+  }
+}
