8b89420da478b56024369c55161847265b4203a0
Peter Donald <peter@realityforge.org>
2019-08-09 12:46:37 +1000

    Add missing import

--- src/main/java/zifnab/hdf/DataElement.java
+++ src/main/java/zifnab/hdf/DataElement.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
