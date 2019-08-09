55844b927bb5500a26f503c34cb6ff6e370b20af
Peter Donald <peter@realityforge.org>
2019-08-09 13:22:03 +1000

    Remove unused constructor

--- src/main/java/zifnab/hdf/DataParseException.java
+++ src/main/java/zifnab/hdf/DataParseException.java
@@ -2,7 +2,6 @@ package zifnab.hdf;
 
 import java.util.Objects;
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 
 public final class DataParseException
   extends Exception
@@ -13,14 +12,6 @@ public final class DataParseException
   public DataParseException( @Nonnull final String message, @Nonnull final SourceLocation location )
   {
     super( Objects.requireNonNull( message ) );
-    _location = Objects.requireNonNull( location );
-  }
-
-  public DataParseException( @Nonnull final String message,
-                             @Nullable final Throwable cause,
-                             @Nonnull final SourceLocation location )
-  {
-    super( Objects.requireNonNull( message ), cause );
     _location = Objects.requireNonNull( location );
   }
 
