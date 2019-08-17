515792b56f2c970e7222ed03fe8efa3c003dd982
Peter Donald <peter@realityforge.org>
2019-08-17 20:58:45 +1000

    Remove unused field

--- src/main/java/zifnab/config/Position.java
+++ src/main/java/zifnab/config/Position.java
@@ -1,11 +1,7 @@
 package zifnab.config;
 
-import javax.annotation.Nonnull;
-
 public final class Position
 {
-  @Nonnull
-  public static final Position ORIGIN = new Position( 0, 0 );
   private final double x;
   private final double y;
 
