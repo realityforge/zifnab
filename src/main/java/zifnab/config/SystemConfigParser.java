31a874f2228973357ac05d5d9a14fa1c52a52635
Peter Donald <peter@realityforge.org>
2019-08-20 17:26:00 +1000

    Inline code and remove asserts on element name to simplify code

--- src/main/java/zifnab/config/SystemConfigParser.java
+++ src/main/java/zifnab/config/SystemConfigParser.java
@@ -36,37 +36,48 @@ final class SystemConfigParser
       switch ( name )
       {
         case "pos":
-          parsePos( config, element );
+          element.assertTokenCount( 3 );
+          config.setPos( new Position( element.getDoubleAt( 1 ), element.getDoubleAt( 2 ) ) );
           break;
         case "government":
-          parseGovernment( config, element );
+          element.assertTokenCount( 2 );
+          config.setGovernment( element.getStringAt( 1 ) );
           break;
         case "haze":
-          parseHaze( config, element );
+          element.assertTokenCount( 2 );
+          config.setHaze( element.getStringAt( 1 ) );
           break;
         case "music":
-          parseMusic( config, element );
+          element.assertTokenCount( 2 );
+          config.setMusic( element.getStringAt( 1 ) );
           break;
         case "habitable":
-          parseHabitable( config, element );
+          element.assertTokenCount( 2 );
+          config.setHabitable( element.getDoubleAt( 1 ) );
           break;
         case "belt":
-          parseBelt( config, element );
+          element.assertTokenCount( 2 );
+          config.setBelt( element.getDoubleAt( 1 ) );
           break;
         case "link":
-          parseLink( config, element );
+          element.assertTokenCount( 2 );
+          config.addLink( element.getStringAt( 1 ) );
           break;
         case "asteroids":
-          parseAsteroids( config, element );
+          element.assertTokenCount( 4 );
+          config.addAsteroid( element.getStringAt( 1 ), element.getIntAt( 2 ), element.getDoubleAt( 3 ) );
           break;
         case "minables":
-          parseMinables( config, element );
+          element.assertTokenCount( 4 );
+          config.addMinable( element.getStringAt( 1 ), element.getIntAt( 2 ), element.getDoubleAt( 3 ) );
           break;
         case "trade":
-          parseTrade( config, element );
+          element.assertTokenCount( 3 );
+          config.addTrade( element.getStringAt( 1 ), element.getIntAt( 2 ) );
           break;
         case "fleet":
-          parseFleet( config, element );
+          element.assertTokenCount( 3 );
+          config.addFleet( element.getStringAt( 1 ), element.getIntAt( 2 ) );
           break;
         case "object":
           config.addObject( parseObject( element ) );
@@ -80,7 +91,6 @@ final class SystemConfigParser
   @Nonnull
   private static SystemConfig.StellarObject parseObject( @Nonnull final DataElement element )
   {
-    element.assertTokenName( "object" );
     element.assertTokenCountRange( 1, 2 );
     final String name = 2 == element.getTokenCount() ? element.getStringAt( 1 ) : null;
     final SystemConfig.StellarObject object = new SystemConfig.StellarObject( name );
@@ -97,16 +107,20 @@ final class SystemConfigParser
       switch ( name )
       {
         case "sprite":
-          parseObjectSprite( object, element );
+          element.assertTokenCount( 2 );
+          object.setSprite( element.getStringAt( 1 ) );
           break;
         case "distance":
-          parseObjectDistance( object, element );
+          element.assertTokenCount( 2 );
+          object.setDistance( element.getDoubleAt( 1 ) );
           break;
         case "period":
-          parseObjectPeriod( object, element );
+          element.assertTokenCount( 2 );
+          object.setPeriod( element.getDoubleAt( 1 ) );
           break;
         case "offset":
-          parseObjectOffset( object, element );
+          element.assertTokenCount( 2 );
+          object.setOffset( element.getDoubleAt( 1 ) );
           break;
         case "object":
           object.addObject( parseObject( element ) );
@@ -115,114 +129,5 @@ final class SystemConfigParser
           throw new DataAccessException( "Unexpected data element named '" + name + "'", element.getLocation() );
       }
     }
-  }
-
-  private static void parseObjectSprite( @Nonnull final SystemConfig.StellarObject object,
-                                         @Nonnull final DataElement element )
-  {
-    element.assertTokenName( "sprite" );
-    element.assertTokenCount( 2 );
-    object.setSprite( element.getStringAt( 1 ) );
-  }
-
-  private static void parseObjectDistance( @Nonnull final SystemConfig.StellarObject object,
-                                           @Nonnull final DataElement element )
-  {
-    element.assertTokenName( "distance" );
-    element.assertTokenCount( 2 );
-    object.setDistance( element.getDoubleAt( 1 ) );
-  }
-
-  private static void parseObjectPeriod( @Nonnull final SystemConfig.StellarObject object,
-                                         @Nonnull final DataElement element )
-  {
-    element.assertTokenName( "period" );
-    element.assertTokenCount( 2 );
-    object.setPeriod( element.getDoubleAt( 1 ) );
-  }
-
-  private static void parseObjectOffset( @Nonnull final SystemConfig.StellarObject object,
-                                         @Nonnull final DataElement element )
-  {
-    element.assertTokenName( "offset" );
-    element.assertTokenCount( 2 );
-    object.setOffset( element.getDoubleAt( 1 ) );
-  }
-
-  private static void parseFleet( @Nonnull final SystemConfig config, @Nonnull final DataElement element )
-  {
-    element.assertTokenName( "fleet" );
-    element.assertTokenCount( 3 );
-    config.addFleet( element.getStringAt( 1 ), element.getIntAt( 2 ) );
-  }
-
-  private static void parseTrade( @Nonnull final SystemConfig config, @Nonnull final DataElement element )
-  {
-    element.assertTokenName( "trade" );
-    element.assertTokenCount( 3 );
-    config.addTrade( element.getStringAt( 1 ), element.getIntAt( 2 ) );
-  }
-
-  private static void parseMinables( @Nonnull final SystemConfig config, @Nonnull final DataElement element )
-  {
-    element.assertTokenName( "minables" );
-    element.assertTokenCount( 4 );
-    config.addMinable( element.getStringAt( 1 ), element.getIntAt( 2 ), element.getDoubleAt( 3 ) );
-  }
-
-  private static void parseAsteroids( @Nonnull final SystemConfig config, @Nonnull final DataElement element )
-  {
-    element.assertTokenName( "asteroids" );
-    element.assertTokenCount( 4 );
-    config.addAsteroid( element.getStringAt( 1 ), element.getIntAt( 2 ), element.getDoubleAt( 3 ) );
-  }
-
-  private static void parseLink( @Nonnull final SystemConfig config, @Nonnull final DataElement element )
-  {
-    element.assertTokenName( "link" );
-    element.assertTokenCount( 2 );
-    config.addLink( element.getStringAt( 1 ) );
-  }
-
-  private static void parseBelt( @Nonnull final SystemConfig config, @Nonnull final DataElement element )
-  {
-    element.assertTokenName( "belt" );
-    element.assertTokenCount( 2 );
-    config.setBelt( element.getDoubleAt( 1 ) );
-  }
-
-  private static void parseHabitable( @Nonnull final SystemConfig config, @Nonnull final DataElement element )
-  {
-    element.assertTokenName( "habitable" );
-    element.assertTokenCount( 2 );
-    config.setHabitable( element.getDoubleAt( 1 ) );
-  }
-
-  private static void parseGovernment( @Nonnull final SystemConfig config, @Nonnull final DataElement element )
-  {
-    element.assertTokenName( "government" );
-    element.assertTokenCount( 2 );
-    config.setGovernment( element.getStringAt( 1 ) );
-  }
-
-  private static void parseHaze( @Nonnull final SystemConfig config, @Nonnull final DataElement element )
-  {
-    element.assertTokenName( "haze" );
-    element.assertTokenCount( 2 );
-    config.setHaze( element.getStringAt( 1 ) );
-  }
-
-  private static void parseMusic( @Nonnull final SystemConfig config, @Nonnull final DataElement element )
-  {
-    element.assertTokenName( "music" );
-    element.assertTokenCount( 2 );
-    config.setMusic( element.getStringAt( 1 ) );
-  }
-
-  private static void parsePos( @Nonnull final SystemConfig config, @Nonnull final DataElement element )
-  {
-    element.assertTokenName( "pos" );
-    element.assertTokenCount( 3 );
-    config.setPos( new Position( element.getDoubleAt( 1 ), element.getDoubleAt( 2 ) ) );
   }
 }
